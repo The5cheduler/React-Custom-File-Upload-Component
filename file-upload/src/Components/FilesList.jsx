a4db78cd11d777b6ac31b3f31522b55622573a5c
Pk_Patel <parthpatel9414@gmail.com>
2022-12-18 18:48:06 -0500

    changes at 6:47 pm

--- file-upload/src/Components/FilesList.jsx
+++ file-upload/src/Components/FilesList.jsx
@@ -1,9 +1,115 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
+import ProcessImage from "./ProcessImage";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import { useCallback } from "react";
 
-function FilesList(props) {
+function FilesList({ updateList }) {
+  const URI = "https://api.uploadcare.com/files/";
+
+  const headers = {
+    Authorization: `Uploadcare.Simple ${process.env.REACT_APP_UPLOADCARE_API_PUBLIC_KEY}:${process.env.REACT_APP_UPLOADCARE_API_SECRET_KEY}`,
+  };
+  const [loading, setLoading] = useState(true);
+  const [files, setFiles] = useState([]);
+  const [fileToProcess, setFileToProcess] = useState(null);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    console.log(headers)
+    fetch(URI, { headers })
+      .then((response) => response.json())
+      .then((data) => {
+        setFiles(data.results);
+        setLoading(false);
+      });
+  }, [updateList]);
+
+  const deleteFile = (fileId) => {
+    const URI = `https://api.uploadcare.com/files/${fileId}/`;
+    const options = {
+      method: "DELETE",
+      headers: headers,
+    };
+    fetch(URI, options)
+      .then((res) => res.json())
+      .then((data) => {
+        // filter out deleted file
+        const newFiles = files.filter((file) => file.uuid !== fileId);
+        setFiles(newFiles);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const editFile = (file) => {
+    console.log(file);
+    setFileToProcess(file);
+    handleShow();
+  };
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (files.length === 0) {
+    return <p className="no-file">So empty here, please upload.</p>;
+  }
+
   return (
-    <div>FilesList</div>
-  )
+    <div className="file-list">
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Process Image</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ProcessImage file={fileToProcess} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <h1>Gallery</h1>
+      <ul className="uc-list-images">
+        {files.map((file) => (
+          <ImageCard 
+            key={file.uuid} 
+            file={file} 
+            deleteFile={deleteFile}
+            editFile={editFile} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const ImageCard = ({ file, deleteFile, editFile }) => {
+  return (
+    <li className="item">
+      <div className="header">
+        <span className="name">{file.original_filename}</span>
+        <div className="actions">
+          <span className="icon" onClick={(image) => editFile(file)}>
+            <FiEdit />
+          </span>
+          <span className="icon" onClick={(id) => deleteFile(file.uuid)}>
+            <FiTrash2 />
+          </span>
+        </div>
+      </div>
+      <div className="file">
+        <img src={file.original_file_url} alt={file.original_filename} />
+      </div>
+    </li>
+  );
 }
 
 export default FilesList
---
+++ file-upload/src/Components/ProcessImage.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+const ProcessImage = ({file}) => {
+  console.log(file);
+  const CDN_URI = `https://ucarecdn.com/${file.uuid}/-/preview/-/quality/smart/`;
+  const [imageSrc, setImageSrc] = useState(CDN_URI);
+
+
+  return(
+    <div>
+      <div className="uc-ip-preview">
+        <img src={imageSrc} alt={file.original_filename} />
+      </div>
+      <div className="uc-ip-actions-grp">
+        <div className="uc-ip-actions">
+          Corp
+          <button onClick={() => setImageSrc(`${CDN_URI}-/crop/2000x1325/center/`)}>Corp Center</button>
+          <button onClick={() => setImageSrc(`${CDN_URI}-/crop/4:3/`)}>Corp Ratio</button>
+          <button onClick={() => setImageSrc(`${CDN_URI}-/crop/face/1:1/`)}>Corp Face</button>
+          <button onClick={() => setImageSrc(`${CDN_URI}-/scale_crop/400x800/smart/`)}>Smart Corp</button>
+          <button onClick={() => setImageSrc(`${CDN_URI}`)}>Set Original</button>
+        </div>
+        <div className="uc-ip-actions">
+          Filter
+          <button onClick={() => setImageSrc(`${CDN_URI}-/filter/lavra/`)}>Lavra</button>
+          <button onClick={() => setImageSrc(`${CDN_URI}-/filter/pamaya/`)}>Pamaya</button>
+          <button onClick={() => setImageSrc(`${CDN_URI}-/filter/sewen/`)}>Sewen</button>
+          <button onClick={() => setImageSrc(`${CDN_URI}-/filter/vevera/`)}>Vevera</button>
+          <button onClick={() => setImageSrc(`${CDN_URI}-/filter/namala/`)}>Namala</button>
+          <button onClick={() => setImageSrc(`${CDN_URI}`)}>Set Original</button>
+        </div>
+        <div className="uc-ip-actions">
+          Rotate & Flip
+          <button onClick={() => setImageSrc(`${CDN_URI}-/rotate/90/`)}>Rotate 90 degree</button>
+          <button onClick={() => setImageSrc(`${CDN_URI}-/rotate/180/`)}>Rotate 180 degree</button>
+          <button onClick={() => setImageSrc(`${CDN_URI}-/flip/`)}>Flip It</button>
+          <button onClick={() => setImageSrc(`${CDN_URI}-/mirror/`)}>Mirror It</button>
+          <button onClick={() => setImageSrc(`${CDN_URI}`)}>Set Original</button>
+        </div>
+        <div className="uc-ip-actions">
+          Blur & Sharpen
+          <button onClick={() => setImageSrc(`${CDN_URI}-/blur_region/faces/`)}>Blur Face</button>
+          <button onClick={() => setImageSrc(`${CDN_URI}-/sharp/10/`)}>Sharp 10</button>
+          <button onClick={() => setImageSrc(`${CDN_URI}-/sharp/20/`)}>Sharp 20</button>
+          <button onClick={() => setImageSrc(`${CDN_URI}`)}>Set Original</button>
+        </div>
+      </div>
+    </div>  
+  )
+}
+
+export default React.memo(ProcessImage);
